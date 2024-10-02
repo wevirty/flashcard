9fc7228989eec9bd1702dd2dc6f8d79b804a3500
wevirty <wevirty.oliveira@escola.pr.gov.br>
2024-10-02 14:20:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Título 1',
+    'Pergunta 1',
+    'Resposta 1'
+)
+
+criaCartao(
+    'Título 2',
+    'Pergunta 2',
+    'Resposta 2'
+)
+
+criaCartao(
+    'Título 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Título 4',
+    'Pergunta 4',
+    'Resposta 4'
+)
+
+criaCartao(
+    'Título 5',
+    'Pergunta 5',
+    'Resposta 5'
+)
+
+criaCartao(
+    'Título 6',
+    'Pergunta 6',
+    'Resposta 6'
+)
+
+criaCartao(
+    'Título 7',
+    'Pergunta 7',
+    'Resposta 7'
+)
+
+criaCartao(
+    'Título 8',
+    'Pergunta 8',
+    'Resposta 8'
+)
+
+criaCartao(
+    'Título 9',
+    'Pergunta 9',
+    'Resposta 9'
+)
+
+criaCartao(
+    'Título 10',
+    'Pergunta 10',
+    'Resposta 10'
+)
